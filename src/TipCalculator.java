f42289dee4e7c34a46e7093df75f444b918b36cb
SameeK2 <sameek2@nycstudents.net>
2023-09-27 12:24:24 -0400

    Added decimal format and rounding

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,26 +1,32 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
 public class TipCalculator {
     public static void main(String[] args) {
+        String pattern = "0.00";
+        DecimalFormat numberFormat = new DecimalFormat(pattern);
         Scanner scan = new Scanner(System.in);
+
         //Gets information from the user
         System.out.print("What was the total bill? ");
         double totalBill = scan.nextDouble();
         System.out.print("What is the percentage that you would like to tip? ");
-        int tipPercentage = scan.nextInt();
+        double tipPercentage = scan.nextInt();
         System.out.print("How many people are in your group? ");
         int totalPeople = scan.nextInt();
+
         //Calculates the outputs
-        double tipAmount = totalBill * tipPercentage / 100; //Finds out the amount the user has to tip
+        double tipAmount = totalBill * (tipPercentage / 100); //Finds out the amount the user has to tip
         double totalBillWithTip = totalBill + tipAmount;    //Calculates the total bill including tip
         double tipPerPerson = tipAmount / totalPeople;  //How much tip each person in the group has to pay
         double totalPaymentPerPerson = totalBillWithTip / totalPeople;  //How much in total does each person in the group have to pay
         //Prints the outputs
-        System.out.println("Bill: " + totalBill);
+
+        System.out.println("Bill: $" + numberFormat.format(totalBill));
         System.out.println("Tip Percentage: " + tipPercentage + "%");
         System.out.println("Number of People: " + totalPeople);
-        System.out.println("Tip Amount: " + tipAmount);
-        System.out.println("Total Bill (with Tip): " + totalBillWithTip);
-        System.out.println("Tip Per Person: " + tipPerPerson);
-        System.out.println("Total Per Person: " + totalPaymentPerPerson);
+        System.out.println("Tip Amount: $" + numberFormat.format(tipAmount + .005));    //Add .005 to the values to round the decimals correctly
+        System.out.println("Total Bill (with Tip): $" + numberFormat.format(totalBillWithTip + .005));
+        System.out.println("Tip Per Person: $" + numberFormat.format(tipPerPerson + .005));
+        System.out.println("Total Per Person: $" + numberFormat.format(totalPaymentPerPerson + .005));
     }
 }
